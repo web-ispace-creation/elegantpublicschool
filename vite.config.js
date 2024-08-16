5032eb06edd86ee8e1391f2264d50d96ce563fbe
Ramkiran <ramkiran152@gmail.com>
2024-08-16 10:21:17 +0530

    added preview cmd in package.json and add scss and js files in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.scss',
+                'resources/css/user.scss',
+                'resources/css/admin.scss',
+                'resources/js/app.js',
+                'resources/js/user.js',
+                'resources/js/admin.js'
+                ],
             refresh: true,
         }),
     ],
