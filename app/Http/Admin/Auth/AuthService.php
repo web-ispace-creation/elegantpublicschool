e4303e7a0150fc9cde80c4507367f1f94abdd87b
Ramkiran <ramkiran152@gmail.com>
2024-08-19 09:54:14 +0530

    otp

--- app/Http/Admin/Auth/AuthService.php
+++ app/Http/Admin/Auth/AuthService.php
@@ -116,7 +116,6 @@ class AuthService
     public function sendMail($email)
     {
         $otp = rand(100000, 999999);
-        $otp = 789456;
         Session::put('adminAuthOtp', $otp);
         Notification::route('mail', $email)->notify(new AdminLogInOTP($otp,$email));
         return 200;
